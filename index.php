18fb10976cada6e49e0f5f39dc636d47ddc324ff
cristianza <lazarod@uninorte.edu.co>
2018-09-26 01:07:58 -0500

    yaaa

--- index.php
+++ index.php
@@ -1,13 +1,45 @@
 <?php
-
   require "vendor/autoload.php";
-
+	class Conectar{
+			public static function conexion(){
+				$usr="root";
+				$psswrd="";
+				try{
+					$conexion = new PDO('mysql:host=localhost;dbname=hoteles_db',$usr,$psswrd);
+				}catch(Exception $e){
+					die("Error" .$e->getMessage());
+					echo "Linea del error" . $e->getLine();
+				}
+				return $conexion;
+			}
+		}
   $app = new \Slim\App;
 
   $app->get('/', function () {
-
-    echo "hello world";
-
+	  $db = Conectar::conexion();
+	  $consulta = $db->prepare("SELECT * FROM `informacion` WHERE state = 'KERALA' and PHONE = '0477-2230767'");
+	  $consulta->execute();
+	  $resultado = $consulta->fetchAll();
+	  $items = $consulta->rowCount();
+	  for ($i=0; $i <= ($items-1); $i++) { 
+		  echo $resultado[$i]['HOTEL NAME'];
+		  echo nl2br("\n");
+		  echo $resultado[$i]['ADDRESS'];
+		  echo nl2br("\n");
+		  echo $resultado[$i]['STATE']; 
+		  echo nl2br("\n");
+		  echo $resultado[$i]['PHONE'];
+		  echo nl2br("\n");
+		  echo $resultado[$i]['FAX'];
+		  echo nl2br("\n");
+		  echo $resultado[$i]['EMAIL ID'];
+		  echo nl2br("\n");
+		  echo $resultado[$i]['WEBSITE'];
+		  echo nl2br("\n");
+		  echo $resultado[$i]['TYPE'];
+		  echo nl2br("\n");
+		  echo $resultado[$i]['Rooms'];
+	  }
   });
 
   $app->get('/cincoestrellas', function () {
