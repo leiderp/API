5bf0865304c68d926e2dea92adeb922e96616336
cristianza <lazarod@uninorte.edu.co>
2018-10-09 14:43:23 -0500

    Punto 1 listo

--- index.php
+++ index.php
@@ -23,15 +23,28 @@ use \Psr\Http\Message\ResponseInterface as Response;
 	  
   });
 
-  $app->get('/consulta/{p}', function (Request $request, Response $response) {
-	  
-	  $t = $request->getAttribute('p');
+  $app->get('/{c}/{n}', function (Request $request, Response $response) {
+	  $n = $request->getAttribute('n');
+	  $c = $request->getAttribute('c');
 	  $db = Conectar::conexion();
-	  $consulta = $db->prepare("SELECT * FROM `informacion` WHERE HOTEL_NAME = '$t'");
+	  if($c == 'SIZE'){
+		  if($n == 'SMALL'){
+			  $consulta = $db->prepare("SELECT * FROM `informacion` WHERE Rooms >= 10 and Rooms <= 50 ");
+		  }else{
+			  if($n == 'MEDIUM'){
+				  $consulta = $db->prepare("SELECT * FROM `informacion` WHERE Rooms >= 51 and Rooms <= 100 ");
+			  }else{
+				  if($n == 'LARGE'){
+					  $consulta = $db->prepare("SELECT * FROM `informacion` WHERE Rooms > 100 ");
+				  }
+			  }
+		  }
+	  }else{
+		  $consulta = $db->prepare("SELECT * FROM `informacion` WHERE $c = '$n'");
+	  }
 	  $consulta->execute();
 	  $resultado = $consulta->fetchAll(PDO::FETCH_OBJ);
-	  echo (json_encode($resultado));
-	  
+      echo (json_encode($resultado));
   });
 
   $app->get('/awita', function () {
@@ -40,7 +53,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
   });
 
-  $app->get('/cat', function () {
+  $app->POST('/cat', function (Request $request, Response $response) {
 
     echo "c'est une prueve";
 
