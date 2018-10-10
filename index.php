3444b3adebefd91c3096816920f825f73e839860
cristianza <lazarod@uninorte.edu.co>
2018-10-10 01:09:49 -0500

    Commit #0

--- index.php
+++ index.php
@@ -19,11 +19,62 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
   $app->get('/', function () {
 	  
+	  ?>
+<!doctype html>
+<head>
+	
+</head>
+
+<body>
+	<link href="c.css" rel="stylesheet" type="text/css">
+	<script src="j.json"></script>
+	<div id="registration-form">
+	<div class='fieldset'>
+    <legend>Registro</legend>
+		<form action="#" method="post" data-validate="parsley">
+			<div class='row'>
+				<label for="email">E-mail</label>
+				<input type="text" placeholder="E-mail"  name='email' data-required="true" data-type="email" data-error-message="Your E-mail is required"  id="em">
+			</div>
+			<div class='row'>
+				<label for='firstname'>Name</label>
+				<input type="text" placeholder="First" name='firstname' id='firstname' data-required="true" data-error-message="Your First Name is required"  id="nm">
+			</div>
+			<div class='row'>
+				<label for="cemail">Last name</label>
+				<input type="text" placeholder="Last name" name='lastname' data-required="true" data-error-message="Your E-mail must correspond" id="lnm">
+			</div>
+			<div class='row'>
+				<label for="cemail">Address</label>
+				<input type="text" placeholder="Address" name='address' data-required="true" data-error-message="Your E-mail must correspond"  id="ad">
+			</div>
+			<div class='row'>
+				<label for="cemail">Passaword</label>
+				<input type="text" placeholder="Confirm your E-mail" name='cemail' data-required="true" data-error-message="Your E-mail must correspond" id="ps">
+			</div>
+			<input type="submit" value="Register" onClick="prueba();" >
+		</form>
+	</div>
+</div>
+
+</body>
+      
+</html>
+
+<script>
+	
+	
+
+
+
+</script>
+<?php
+	  
 	
 	  
   });
 
-  $app->get('/{c}/{n}', function (Request $request, Response $response) {
+  $app->get('/CONSULTAS/{c}/{n}', function (Request $request, Response $response) {
 	  $n = $request->getAttribute('n');
 	  $c = $request->getAttribute('c');
 	  $db = Conectar::conexion();
@@ -47,16 +98,12 @@ use \Psr\Http\Message\ResponseInterface as Response;
       echo (json_encode($resultado));
   });
 
-  $app->get('/awita', function () {
-
-    echo "papi";
-
-  });
-
-  $app->POST('/cat', function (Request $request, Response $response) {
-
-    echo "c'est une prueve";
-
+  $app->post('/ticket/new', function (Request $request, Response $response) {
+	  
+	  
+	  
+	  
+	  
   });
 
 
