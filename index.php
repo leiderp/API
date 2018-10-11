868ff1cec84712c67238adc00d100e273001851d
andres9906 <stivenaereo@gmail.com>
2018-10-11 15:24:51 -0500

    Creacion de usuario
    creacion de usuario

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
   require "vendor/autoload.php";
   require "Modelo/Conectar.php";
-	
+
   use \Psr\Http\Message\ServerRequestInterface as Request;
   use \Psr\Http\Message\ResponseInterface as Response;
   $app = new \Slim\App;
 
   $app->get('/', function () {
-	  
+
   });
 
   $app->get('/CONSULTAS/{c}/{n}', function (Request $request, Response $response) {
@@ -35,12 +35,39 @@
   });
 
   $app->post('/ticket/new', function (Request $request, Response $response) {
-	  
-	  
-	  
-	  
-	  echo("dnjwndjen");
+
+    	  echo("dnjwndjen");
   });
+
+
+  $app->post('/usuario/add', function (Request $request, Response $response) {
+      $name = $request->getParam('name');
+      $last_name = $request->getParam('last_name');
+      $address = $request->getParam('address');
+      $email = $request->getParam('email');
+      $password = $request->getParam('password');
+
+
+      $sql = "INSERT INTO usuarios (name, last_name, address, email, password) VALUES
+      (:name, :last_name, :address, :email, :password)";
+  	  $db = Conectar::conexion();
+
+      $stmt = $db->prepare($sql);
+
+      $stmt-> bindParam(':name', $name);
+      $stmt-> bindParam(':last_name', $last_name);
+      $stmt-> bindParam(':address', $address);
+      $stmt-> bindParam(':email', $email);
+      $stmt-> bindParam(':password', $password);
+
+  	  $stmt->execute();
+
+      echo '{"response" : "User created"}';
+      return $response;
+    });
+
+
+  /*$resultado = $consulta->fetchAll(PDO::FETCH_OBJ);*/
 
 
   $app->run();
