a0627cbaac9e2f0b51c4dfa957f7ca96e515a645
cristianza <lazarod@uninorte.edu.co>
2018-10-11 13:55:00 -0500

    borre el formulario

--- index.php
+++ index.php
@@ -7,56 +7,7 @@
   $app = new \Slim\App;
 
   $app->get('/', function () {
-	  ?>
-	  <html>
-	<head>
-		<title>Formulario de Registro</title>
-		<link rel="stylesheet" type="text/css" href="Vista/bootstrap/css/bootstrap.min.css">
-	</head>
-	<body>
-	<?php include "Controlador/navbar.php"; ?>
-<div class="container">
-<div class="row">
-<div class="col-md-6">
-		<h2>Registro</h2>
-
-		<form role="form" name="registro" action="Controlador/registro.php" method="post">
-		  <div class="form-group">
-		    <label for="username">Name</label>
-		    <input type="text" class="form-control" id="username" name="username" placeholder="Nombre de usuario">
-		  </div>
-		  <div class="form-group">
-		    <label for="fullname">Last name</label>
-		    <input type="text" class="form-control" id="fullname" name="fullname" placeholder="Nombre Completo">
-		  </div>
-		  <div class="form-group">
-		    <label for="address">Address</label>
-		    <input type="text" class="form-control" id="address" name="address" placeholder="Nombre Completo">
-		  </div>
-		  <div class="form-group">
-		    <label for="email">Correo Electronico</label>
-		    <input type="email" class="form-control" id="email" name="email" placeholder="Correo Electronico">
-		  </div>
-		  <div class="form-group">
-		    <label for="password">Contrase&ntilde;a</label>
-		    <input type="password" class="form-control" id="password" name="password" placeholder="Contrase&ntilde;a">
-		  </div>
-		  <div class="form-group">
-		    <label for="confirm_password">Confirmar Contrase&ntilde;a</label>
-		    <input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="Confirmar Contrase&ntilde;a">
-		  </div>
-
-		  <button type="submit" class="btn btn-default">Registrar</button>
-		</form>
-		</div>
-		</div>
-		</div>
-
-		<script src="Controlador/js/valida_registro.js"></script>
-	</body>
-</html>
 	  
-	  <?php
   });
 
   $app->get('/CONSULTAS/{c}/{n}', function (Request $request, Response $response) {
