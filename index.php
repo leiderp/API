9ca6edd281d36148f0c9d553506f00afb687aa22
cristianza <lazarod@uninorte.edu.co>
2018-10-12 00:31:12 -0500

    update listo - criz

--- index.php
+++ index.php
@@ -10,18 +10,18 @@
 
   });
 
-  $app->get('/CONSULTAS/{c}/{n}', function (Request $request, Response $response) {
-	  $n = $request->getAttribute('n');
-	  $c = $request->getAttribute('c');
+  $app->get('/consultas/{atributo}/{nombre}', function (Request $request, Response $response) {
+	  $n = $request->getAttribute('nombre');
+	  $c = $request->getAttribute('atributo');
 	  $db = Conectar::conexion();
-	  if($c == 'SIZE'){
-		  if($n == 'SMALL'){
+	  if($c == 'size'){
+		  if($n == 'small'){
 			  $consulta = $db->prepare("SELECT * FROM `informacion` WHERE Rooms >= 10 and Rooms <= 50 ");
 		  }else{
-			  if($n == 'MEDIUM'){
+			  if($n == 'medium'){
 				  $consulta = $db->prepare("SELECT * FROM `informacion` WHERE Rooms >= 51 and Rooms <= 100 ");
 			  }else{
-				  if($n == 'LARGE'){
+				  if($n == 'large'){
 					  $consulta = $db->prepare("SELECT * FROM `informacion` WHERE Rooms > 100 ");
 				  }
 			  }
@@ -33,12 +33,6 @@
 	  $resultado = $consulta->fetchAll(PDO::FETCH_OBJ);
       echo (json_encode($resultado));
   });
-
-  $app->post('/ticket/new', function (Request $request, Response $response) {
-
-    	  echo("dnjwndjen");
-  });
-
 
   $app->post('/usuario/add', function (Request $request, Response $response) {
       $name = $request->getParam('name');
@@ -72,10 +66,38 @@
 
     });
 
+$app->put('/usuario/update/{id}', function (Request $request, Response $response) {
+	  $id = $request->getAttribute('id');
+	  $name = $request->getParam('name');
+      $last_name = $request->getParam('last_name');
+      $address = $request->getParam('address');
+      $email = $request->getParam('email');
+      $password = $request->getParam('password');
 
-
-  /*$resultado = $consulta->fetchAll(PDO::FETCH_OBJ);*/
-
+  	  $db = Conectar::conexion();
+	$sql = "UPDATE usuarios SET
+				name = :name,
+				last_name = :last_name,
+				address = :address,
+				email = :email,
+				password = :password
+			WHERE id = $id";
+	try{
+		
+		$stmt = $db->prepare($sql);
+		$stmt-> bindParam(':name', $name);
+		$stmt-> bindParam(':last_name', $last_name);
+		$stmt-> bindParam(':address', $address);
+	    $stmt-> bindParam(':email', $email);
+		$stmt-> bindParam(':password', $password);
+  	    $stmt->execute();
+		
+	}catch(PDOException $e){
+		echo '{"error": {"text": '.$e->getMessage().'}}';
+		
+	}
+	
+  });
 
   $app->run();
 
